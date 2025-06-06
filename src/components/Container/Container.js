5a8cf3168421a15e7c71fb17beb07dd8a6db0f21
bg4zz <b-inozemtsev@list.ru>
2025-06-06 16:02:28 +0500

    create new elements for React project

--- src/components/Container/Container.js
+++ src/components/Container/Container.js
@@ -1,12 +1,13 @@
 import Task from "../Task/Task";
+import './Container.css'
 
 export default function Container() {
     let tasks = ['Buy milk', 'Go home', 'Go sleep', 'Finish project'];
     return (
-        <div>
+        <div className="container">
             {
-                tasks.map((item) => (
-                    <Task name={item} />
+                tasks.map((item, index) => (
+                    <Task name={item} key={index} />
                 ))
             }
         </div>
--- src/components/Task/Task.js
+++ src/components/Task/Task.js
@@ -1,11 +1,27 @@
+import { useState } from 'react';
 import { PencilFill, TrashFill } from 'react-bootstrap-icons';
+import './Task.css';
 
 export default function Task({ name }) {
+    const [isVisible, setIsVisible] = useState(true);
+    const [count, setCount] = useState(0);
+
+    const RemoveTask = () => {
+        setIsVisible(false);
+        console.log('is remove =(');
+    }
+
+    const EditTask = () => {
+        setCount(count + 1);
+        console.log('is edit!');
+    }
+
     return (
-        <div>
+        <div className={isVisible ? "task" : "hide"}>
             <span>{name}</span>
-            <PencilFill />
-            <TrashFill />
+            <PencilFill className='btn-edit' onClick={EditTask} />
+            <TrashFill className='btn-remove' onClick={RemoveTask} />
+            {count}
         </div>
     );
 }
